9c5faebf6e1f4d8bf448d935641709afa16d0d3c
Tyler Hedegard <tchedegard@gmail.com>
2016-06-01 13:50:29 -0500

    Finish printing out results

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+
 import sqlite3 as lite
 import pandas as pd
 
@@ -5,56 +6,58 @@ import pandas as pd
 con = lite.connect('getting_started.db')
 
 cities = (
-	('New York City', 'NY'),
-	('Boston', 'MA'),
-	('Chicago', 'IL'),
-	('Miami', 'FL'),
-	('Dallas', 'TX'),
-	('Seattle', 'WA'),
+    ('New York City', 'NY'),
+    ('Boston', 'MA'),
+    ('Chicago', 'IL'),
+    ('Miami', 'FL'),
+    ('Dallas', 'TX'),
+    ('Seattle', 'WA'),
     ('Portland', 'OR'),
     ('San Francisco', 'CA'),
     ('Los Angeles', 'CA')
-    )
+)
 
 weather = (
-	('New York City',2013,'July','January',62),
-	('Boston',2013,'July','January',59),
-	('Chicago',2013,'July','January',59),
-	('Miami',2013,'August','January',59),
-	('Dallas',2013,'July','January',59),
-	('Seattle',2013,'July','January',59),
-	('Portland',2013,'July','December',59),
-	('San Francisco',2013,'September','December',59),
-	('Los Angeles',2013,'September','December',59)
-	)
+    ('New York City', 2013, 'July', 'January', 62),
+    ('Boston', 2013, 'July', 'January', 59),
+    ('Chicago', 2013, 'July', 'January', 59),
+    ('Miami', 2013, 'August', 'January', 59),
+    ('Dallas', 2013, 'July', 'January', 59),
+    ('Seattle', 2013, 'July', 'January', 59),
+    ('Portland', 2013, 'July', 'December', 59),
+    ('San Francisco', 2013, 'September', 'December', 59),
+    ('Los Angeles', 2013, 'September', 'December', 59)
+)
 
 with con:
+    cur = con.cursor()
+    # Create the cities and weather tables
+    cur.execute("DROP TABLE IF EXISTS cities")
+    cur.execute("DROP TABLE IF EXISTS weather")
+    cur.execute("CREATE TABLE cities (name text, state text)")
+    cur.execute(
+        "CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_high integer)")
 
-	cur = con.cursor()
-	# Create the cities and weather tables (HINT: first pass the statement DROP TABLE IF EXISTS <table_name>; to remove the table before you execute the CREATE TABLE ... statement)
-	cur.execute("DROP TABLE IF EXISTS cities")
-	cur.execute("DROP TABLE IF EXISTS weather")
-	cur.execute("CREATE TABLE cities (name text, state text)")
-	cur.execute("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_high integer)")
-	
-	# Insert data into the two tables
-	cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
-	cur.executemany("INSERT INTO weather VALUES(?,?,?,?,?)", weather)
+    # Insert data into the two tables
+    cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
+    cur.executemany("INSERT INTO weather VALUES(?,?,?,?,?)", weather)
 
-	# Join the data together
-	cur.execute("""SELECT name, state FROM cities 
-		INNER JOIN weather ON name = city
-		where warm_month = 'July'""")
+    # Join the data together
+    cur.execute("""SELECT name, state FROM cities
+        INNER JOIN weather ON name = city
+        where warm_month = 'July'""")
 
-	# Load into a pandas DataFrame
-	rows = cur.fetchall()
-  	cols = [desc[0] for desc in cur.description]
-  	df = pd.DataFrame(rows, columns=cols)
+    # Load into a pandas DataFrame
+    rows = cur.fetchall()
+    cols = [desc[0] for desc in cur.description]
+    df = pd.DataFrame(rows, columns=cols)
 
-	# Print out the resulting city and state in a full sentence. 
-	# For example: "The cities that are warmest in July are: Las Vegas, NV, Atlanta, GA..."
-	print(df)
-	warm_cities = 0 #need to build this string with components of df object
-	print("The cities that are warmest in July are: {}").format(warm_cities)
-
+    # Print out the resulting city and state in a full sentence.
+    # For example: "The cities that are warmest in July are: Las Vegas, NV, Atlanta, GA..."
+    warm_cities = []
+    for i in range(df.shape[0]):
+        warm_cities.append([df.get_value(i, 'name'), df.get_value(i, 'state')])
+    print("The cities that are warmest in July are: ")
+    for city in warm_cities:
+        print("\t{}, {}".format(city[0],city[1]))
 
